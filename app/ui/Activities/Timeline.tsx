9cc7495478e4e143004d74242f30b2029f741e04
Taewon Hwang <taewonhwang7@gmail.com>
2024-10-16 17:23:52 -0400

    Changed all picture to jpg format and changed name to image1,2,3

--- app/ui/Activities/Timeline.tsx
+++ app/ui/Activities/Timeline.tsx
@@ -11,9 +11,9 @@ const events = {
     date: "August 31 - September 1st 2024",
     description:
       "The Fondation Communautaire Canadienne-Coréenne du Québec (FCCCQ) has proudly partnered with the AKCSE Quebec YP and YG chapters to host an annual mentorship event. Over the course of two days, 60 participants engaged in mentor presentations and Q&A sessions. This event aims to provide guidance to any students that are uncertain about their career choices or future directions. With the support of 32 mentors, students gain a new perspective on how to navigate their professional journeys.",
-    image1: "/activities/Youth Camp/Youth_Camp_1.jpeg",
-    image2: "/activities/Youth Camp/Youth_Camp_2.jpg",
-    image3: "/activities/Youth Camp/Youth_Camp_3.jpeg",
+    image1: "/activities/Youth Camp/image1.jpg",
+    image2: "/activities/Youth Camp/image2.jpg",
+    image3: "/activities/Youth Camp/image3.jpg",
   },
   "Orientation Night": {
     event: "Orientation Night",
@@ -38,36 +38,36 @@ const events = {
     date: "TBD",
     description:
       "As preparation for hackathon events, students from diverse programs in McGill unleash their creativity and technical skills during a brainstorming of clever applications and websites, which can eventually become an AKCSE summer project. As opposed to hackathons, the event is open to all faculties and majors. Students have the opportunity to gain valuable feedback from professionals in YP Quebec and experience practical tools such as Figma. ",
-    image1: "/activities/Ideathon/Ideathon_1.jpeg",
-    image2: "/activities/Ideathon/Ideathon_2.jpeg",
-    image3: "/activities/Ideathon/Ideathon_3.jpeg",
+    image1: "/activities/Ideathon/image1.jpg",
+    image2: "/activities/Ideathon/image2.jpg",
+    image3: "/activities/Ideathon/image3.jpg",
   },
   "Christmas Party": {
     event: "Christmas Party",
     date: "TBD",
     description:
       "Christmas Event is a festive celebration marking the end of the semester for AKCSE members. This event aims to provide AKCSE members a way to unwind and relax after a stressful semester. This is a fantastic opportunity to bond with fellow AKCSE members and meet new faces as we prepare for an exciting upcoming semester. ",
-    image1: "/activities/Christmas/Christmas_1.png",
-    image2: "/activities/Christmas/Christmas_2.png",
-    image3: "/activities/Christmas/Christmas_3.png",
+    image1: "/activities/Christmas/image1.jpg",
+    image2: "/activities/Christmas/image2.jpg",
+    image3: "/activities/Christmas/image3.jpg",
   },
   "YG Seminar": {
     event: "YG Seminar",
     date: "TBD",
     description:
       "The YG seminar invites professionals from different STEM fields such as medicine, IT, engineering, and natural sciences. This is a great opportunity for students to gain a bigger picture of what it’s like to be in research or industry by listening to their journey and firsthand experience over the years. This is also a great chance for students to directly ask presenters in a more approachable environment. ",
-    image1: "/activities/ot/Christmas_1.png",
-    image2: "/activities/ot/Christmas_2.png",
-    image3: "/activities/ot/Christmas_3.png",
+    image1: "/activities/ot/image1.jpg",
+    image2: "/activities/ot/image2.jpg",
+    image3: "/activities/ot/image3.jpg",
   },
   "Meet Your Mentor": {
     event: "Meet Your Mentor",
     date: "TBD",
     description:
       "Every year, Korean clubs at McGill MECA x KSS x AKCSE hold a mentoring event, aimed at creating a networking space between our Korean students and esteemed individuals from various fields, including management, medicine and health sciences, engineering, law, IT and others. The purpose of this gathering is to foster connections, share experiences, and provide our students with insights into different career paths. ",
-    image1: "/activities/MYM/MYM_1.jpg",
-    image2: "/activities/MYM/MYM_2.jpg",
-    image3: "/activities/MYM/MYM_3.jpg",
+    image1: "/activities/MYM/image1.jpg",
+    image2: "/activities/MYM/image2.jpg",
+    image3: "/activities/MYM/image3.jpg",
   },
 };
 
